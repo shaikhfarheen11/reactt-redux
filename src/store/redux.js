ebb8beefa63948f9e2a4a1d87cc8372963b28fc0
shaikhfarheen11 <farheens554>
2024-01-07 20:14:16 +0530

    first commit

--- src/store/redux.js
+++ src/store/redux.js
@@ -1,33 +1,41 @@
 const redux = require('redux');
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  switch (action.type) {
-    case 'increment':
-      return {
-        counter: state.counter + 1
-      };
-    case 'decrement':
-      return {
-        counter: state.counter - 1
-      };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case 'increment':
+            return {
+                counter: state.counter + 1
+            };
+        case 'decrement':
+            return {
+                counter: state.counter - 1
+            };
+        case 'INCREMENTBY2':
+            return {
+                counter: state.counter + 2
+            };
+        case 'DECREMENTBY2':
+            return {
+                counter: state.counter - 2
+            };
+        default:
+            return state;
+    }
 };
 
 const store = redux.createStore(counterReducer);
 
-console.log(store.getState());
-
 const counterSubscriber = () => {
-  const latestState = store.getState();
-  console.log(latestState);
+    const latestState = store.getState();
+    console.log(latestState);
 };
 
 store.subscribe(counterSubscriber);
 
 for (let i = 0; i < 5; i++) {
-  store.dispatch({ type: 'increment' });
+    store.dispatch({ type: 'increment' });
 }
 
 store.dispatch({ type: 'decrement' });
+store.dispatch({ type: 'INCREMENTBY2' }); 
+store.dispatch({ type: 'DECREMENTBY2' }); 
